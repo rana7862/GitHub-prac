319aa05e3302de9f63b3b102da5373f0642f8ca1
rana7862 <rana.ritik9639@gmail.com>
2025-09-09 15:58:02 +0530

    my first commit

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+create_new_release(){
+    tag_number=$(grep -oP 'releaseNo=\k[^ ]+' " ./releaseNO.eve" )
+    tag_with_prefix="v${tag_number}-testing"
+    gh api \
+    --method POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    /repos/rana7862/GitHub-prac/releases \
+    -F tag_name="$tag_with_prefix" \
+    -F target_commitish="development" \
+    -F name="$tag_with_prefix" \
+    -F draft=false \
+    -F prerelease=false \
+    -F body=@"./modify.txt"
+    echo "Release $tag_with_prefix created successfully."
+    
+
+}
